af6a22dd5c4f81b73faef930a3d1db9811f9d770
Ben Olson <molson5@utk.edu>
2015-06-25 14:08:49 -0400

    Fixed auto compilation of ant and maven

--- compile.sh
+++ compile.sh
@@ -2,11 +2,33 @@
 set -e
 set -o pipefail
 
-# In order to compile this application, simply run `ant`, using the modified `build.xml` in this directory.
-# Of course, the values in `build.xml` are relative to the location of this directory, so if you move it,
-# also change the values in `build.xml`.
+# Get into the directory that this script is stored in
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR
 
-#Compile YCSB itself
+# Make sure ant is ready and compiled
+cd utils
+tar xf apache-ant-1.9.5-src.tar.gz
+cd apache-ant-1.9.5
+sh build.sh -Ddist.dir=$DIR/utils/ant dist
+cd ..
+
+# Set up some nice environment variables
+export ANT_HOME=$DIR/utils/ant
+export M2_HOME=$DIR/utils/maven
+
+# Make sure maven is ready and compiled
+tar xf apache-maven-3.2.5-src.tar.gz
+cd apache-maven-3.2.5
+yes yes | ant
+
+# Get out of the utils directory
+cd ../..
+
+# Now set your PATH accordingly
+export PATH="$PATH:$DIR/utils/ant/bin:$DIR/utils/maven/bin"
+
+# Compile YCSB itself
 cd core
 mvn clean package
 cd ..
