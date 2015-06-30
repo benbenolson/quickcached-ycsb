2533478239ab03d13083df5b7f5e782ae5e76464
Ben Olson <molson5@utk.edu>
2015-06-30 15:10:56 -0400

    Fixed memory to the JVM, added name feature

--- runworkload.sh
+++ runworkload.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] ; then
-  echo "Please provide four arguments:"
-  echo "  - The letter of the workload"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ]; then
+  echo "Please provide five arguments:"
+  echo "  - The name of the run that you're doing."
+  echo "  - The letter of the workload."
   echo "  - The number of records."
   echo "  - The number of threads."
   echo "  - The target number of records for each thread."
+  echo "  - (OPTIONAL) The amount of memory in bytes that the JVM should be allowed to have."
   exit 1
 fi
 
 # Set some nice variables from the arguments
-WORKLOAD=$1
-RECORDCOUNT=$2
-THREADS=$3
-TARGET=$4
+NAME=$1
+WORKLOAD=$2
+RECORDCOUNT=$3
+THREADS=$4
+TARGET=$5
 
 # Make sure the logs directory exists
-mkdir -p logs
+mkdir -p logs/$NAME
 
 # Figure out how much memory we need, assuming 10 fields of 100 bytes each in each record
-MEMORY=$(echo "(($RECORDCOUNT * 100 * 10 * 2) + 0.5) / 1" | bc)
-if [ $MEMORY -gt 110000000000 ]
-then
-  echo "You're using more than 100GB of memory. Are you really sure you want to do that?"
-elif [ $MEMORY -lt 10000000 ]
-then
-  echo "Using the minimum of 10MB of memory."
-  MEMORY=10000000
+# Or use the amount specified by the user
+if [ -z "$6" ]; then
+  MEMORY=110000000000
+  echo "Automatically set the amount of memory being used to $MEMORY"
 else
-  echo "Using $MEMORY bytes of memory."
+  MEMORY=$6
+  if [ $MEMORY -gt 110000000000 ]; then
+    echo "Using the maximum of 110GB of memory."
+    MEMORY=110000000000
+  elif [ $MEMORY -lt 10000000 ]; then
+    echo "Using the minimum of 10MB of memory."
+    MEMORY=10000000
+  else
+    echo "Manually set the amount of memory being used to $MEMORY"
+  fi
 fi
 
 # Start the server
@@ -36,8 +44,9 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 cd quickcached
 exec hotspot -Dappname=QC1 -Xms$MEMORY -Xmx$MEMORY -XX:CompileThreshold=1500 -XX:+UseConcMarkSweepGC \
+  -XX:+PrintHeapAtGC -XX:+PrintGCDetails \
   -jar dist/QuickCached-Server.jar -p 11211 -l 127.0.0.1 \
-  > "../logs/w$WORKLOAD-r$RECORDCOUNT-th$THREADS-t$TARGET-server" 2>&1 &
+  > "../logs/$NAME/w$WORKLOAD-r$RECORDCOUNT-th$THREADS-t$TARGET-m$MEMORY-server" 2>&1 &
 SERVER_PID=$!
 cd ..
 
@@ -46,11 +55,11 @@ sleep 1
 
 echo "Loading the data into the database..."
 bin/ycsb load quickcached -P workloads/workload$WORKLOAD -p recordcount=$RECORDCOUNT \
-  &> "logs/w$WORKLOAD-r$RECORDCOUNT-th$THREADS-t$TARGET-load"
+  &> "logs/$NAME/w$WORKLOAD-r$RECORDCOUNT-th$THREADS-t$TARGET-m$MEMORY-load"
 
 echo "Running the benchmark on the data..."
 bin/ycsb run quickcached -P workloads/workload$WORKLOAD -s -threads $THREADS -target $TARGET \
-  &> "logs/w$WORKLOAD-r$RECORDCOUNT-th$THREADS-t$TARGET-run"
+  &> "logs/$NAME/w$WORKLOAD-r$RECORDCOUNT-th$THREADS-t$TARGET-m$MEMORY-run"
 
 echo "Killing processes associated with the server:"
 echo "  $(ps -o pgid= $SERVER_PID | grep -o '[0-9]*')"
